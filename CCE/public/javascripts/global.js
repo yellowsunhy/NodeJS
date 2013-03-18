96794a390a75ec271f69f67d77ed0b345e4354d3
Yang Huang <huang.yang1@husky.neu.edu>
2013-03-18 13:20:52 -0400

    Just put the website on

--- CCE/public/javascripts/global.js
+++ CCE/public/javascripts/global.js
@@ -1,8 +1,9 @@
 $(function(){
 	// $('#search_box').hint();
-	$('body').prepend('<div class="probe" style="position:fixed;top:200px;z-index:1000;"></div>')
-	$(window).resize(function(){
-		console.log($(window).width())
-		$('.probe').html($(window).width())
-	})
+	// $('body').prepend('<div class="probe" style="position:fixed;top:200px;z-index:1000;"></div>')
+	// $(window).resize(function(){
+	// 	console.log($(window).width())
+	// 	$('.probe').html($(window).width())
+	// })
+	$('#carousel').carousel()
 });
--- CCE/routes/index.js
+++ CCE/routes/index.js
@@ -6,8 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { 
   	title: 'Career Engine',
-  	author: 'Mister Sun Studio',
-  	description: 'Our aim is to help the Chinese students oversea to better find their job',
-  	menus: ["招聘","求职指导","活动","签证","登陆"]
+  	menus: [{name:"求职",submenus:["面试","简历","笔试","网申","行业资讯","公司资讯"]},
+  			{name:"活动"},
+  			{name:"登陆",id:"login"}],
+  	slides: [{headline:"标题一",description:"专注于为华人留学生寻找最优匹配的职位",url:"/images/search_bar_bg_blur.jpg"},
+  			{headline:"Title2",description:"Bla Bla Bla...",url:"/images/search_bar_bg_blur.jpg"},
+  			{headline:"Title3",description:"Bla Bla Bla...",url:"/images/search_bar_bg_blur.jpg"}]
    });
 };
